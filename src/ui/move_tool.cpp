53de5dd2075650eec181ce769eb09d973a25a92d
Joe Wezorek <jwezorek@gmail.com>
2023-05-20 07:51:50 -0700

    stubbed in move tool a little.

--- src/ui/move_tool.cpp
+++ src/ui/move_tool.cpp
@@ -1,34 +1,99 @@
 #include "move_tool.h"
 #include "tool_settings_pane.h"
+#include <unordered_map>
+#include <array>
 
 /*------------------------------------------------------------------------------------------------*/
 
 namespace {
+
+    using move_state = ui::move_tool::move_state;
 
     enum class move_mode {
         none = -1,
         translate = 0,
         rotate,
         pinned_ik,
-        ik_free
+        ik_free,
+        end
     };
 
-    auto k_move_mode = std::to_array<std::tuple< move_mode, QString>>({
-        { move_mode::translate , "translate (free)"},
-        { move_mode::rotate , "rotate (forward kinematics)" },
-        { move_mode::pinned_ik , "translate pinned (ik)" },
-        { move_mode::ik_free , "translate unpinned (ik)" }
+    class abstract_move_tool {
+        move_mode mode_;
+        QString name_;
+    public:
+        abstract_move_tool(move_mode mode, QString name) :
+            mode_(mode), name_(name)
+        {}
+
+        QString name() const { return name_; }
+        move_mode mode() const { return mode_; }
+
+        virtual void mousePressEvent(move_state& ms) = 0;
+        virtual void mouseMoveEvent(move_state& ms) = 0;
+        virtual void mouseReleaseEvent(move_state& ms) = 0;
+    };
+
+    class placeholder_move_tool : public abstract_move_tool {
+    public:
+        placeholder_move_tool(move_mode mode, QString name) :
+            abstract_move_tool(mode, name)
+        {}
+        void mousePressEvent(move_state& ms) override {};
+        void mouseMoveEvent(move_state& ms) override {};
+        void mouseReleaseEvent(move_state& ms) override {};
+    };
+
+    auto k_move_mode = std::to_array<std::unique_ptr<abstract_move_tool>>({
+
+            std::make_unique<placeholder_move_tool>(
+                move_mode::translate ,
+                "translate (free)"
+            ),
+            std::make_unique<placeholder_move_tool>(
+                move_mode::rotate ,
+                "rotate (forward kinematics)"
+            ),
+            std::make_unique<placeholder_move_tool>(
+                 move_mode::pinned_ik ,
+                "translate pinned (ik)"
+            ),
+            std::make_unique<placeholder_move_tool>(
+                move_mode::ik_free ,
+                "translate unpinned (ik)"
+            )
         });
 
-    void rotate_mouse_press(ui::canvas& c, QGraphicsSceneMouseEvent* event) {
-    }
-
-    void rotate_mouse_move(ui::canvas& c, QGraphicsSceneMouseEvent* event) {
-    }
-
-    void rotate_mouse_release(ui::canvas& c, QGraphicsSceneMouseEvent* event) {
+    abstract_move_tool* tool_for_mode(move_mode mode) {
+        static std::unordered_map<move_mode, abstract_move_tool*> tbl;
+        if (tbl.empty()) {
+            auto end = static_cast<int>(move_mode::end);
+            for (int i = 0; i < end; ++i) {
+                tbl[static_cast<move_mode>(i)] = k_move_mode[i].get();
+            }
+        }
+        if (mode == move_mode::none || mode == move_mode::end) {
+            return nullptr;
+        }
+        return tbl.at(mode);
     }
 }
+
+/*------------------------------------------------------------------------------------------------*/
+
+ui::move_tool::move_state::move_state() : 
+    canvas_(nullptr), 
+    anchor_(0,0), 
+    rubber_band_(nullptr), 
+    event_(nullptr) 
+{}
+
+void ui::move_tool::move_state::set(canvas& c, QGraphicsSceneMouseEvent* evnt) {
+    canvas_ = &c;
+    event_ = evnt;
+}
+
+/*------------------------------------------------------------------------------------------------*/
 
 ui::move_tool::move_tool() : abstract_tool("move", "move_icon.png", ui::tool_id::move) {
 
@@ -36,42 +101,27 @@ ui::move_tool::move_tool() : abstract_tool("move", "move_icon.png", ui::tool_id:
 
 void ui::move_tool::mousePressEvent(canvas& c, QGraphicsSceneMouseEvent* event) {
     auto mode = static_cast<move_mode>(btns_->checkedId());
-    switch (mode) {
-    case move_mode::rotate:
-        rotate_mouse_press(c, event);
-        return;
-    default:
-        return;
-    }
+    state_.set(c, event);
+    tool_for_mode(mode)->mousePressEvent(state_);
 }
 
 void ui::move_tool::mouseMoveEvent(canvas& c, QGraphicsSceneMouseEvent* event) {
     auto mode = static_cast<move_mode>(btns_->checkedId());
-    switch (mode) {
-    case move_mode::rotate:
-        rotate_mouse_move(c, event);
-        return;
-    default:
-        return;
-    }
+    state_.set(c, event);
+    tool_for_mode(mode)->mouseMoveEvent(state_);
 }
 
 void ui::move_tool::mouseReleaseEvent(canvas& c, QGraphicsSceneMouseEvent* event) {
     auto mode = static_cast<move_mode>(btns_->checkedId());
-    switch (mode) {
-    case move_mode::rotate:
-        rotate_mouse_release(c, event);
-        return;
-    default:
-        return;
-    }
+    state_.set(c, event);
+    tool_for_mode(mode)->mouseReleaseEvent(state_);
 }
 
 void ui::move_tool::populate_settings_aux(tool_settings_pane* pane) {
     btns_ = std::make_unique<QButtonGroup>();
-    for (const auto& [id, name] : k_move_mode) {
-        auto* btn = new QRadioButton(name);
-        btns_->addButton(btn, static_cast<int>(id));
+    for (const auto& mm : k_move_mode) {
+        auto* btn = new QRadioButton(mm->name());
+        btns_->addButton(btn, static_cast<int>(mm->mode()));
         pane->contents()->addWidget(btn);
     }
 }
--- src/ui/move_tool.h
+++ src/ui/move_tool.h
@@ -4,22 +4,30 @@
 
 namespace ui {
 
-    namespace detail {
-        struct rotate_state {
-
-        };
-    }
-
     class move_tool : public abstract_tool {
-    private:
-        std::unique_ptr<QButtonGroup> btns_;
-        detail::rotate_state rotate_;
     public:
+
+        struct move_state {
+
+            canvas* canvas_;
+            QPointF anchor_;
+            QGraphicsItem* rubber_band_;
+            QGraphicsSceneMouseEvent* event_;
+
+            move_state();
+            void set(canvas& canvas, QGraphicsSceneMouseEvent* event);
+        };
+
         move_tool();
         void mousePressEvent(canvas& c, QGraphicsSceneMouseEvent* event) override;
         void mouseMoveEvent(canvas& c, QGraphicsSceneMouseEvent* event) override;
         void mouseReleaseEvent(canvas& c, QGraphicsSceneMouseEvent* event) override;
         void populate_settings_aux(tool_settings_pane* pane) override;
+
+    private: 
+
+        std::unique_ptr<QButtonGroup> btns_;
+        move_state state_;
     };
 
 }
