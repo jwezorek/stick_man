825eb903408cb7312f98a70349e8830afaa0c1cc
Joe Wezorek <jwezorek@gmail.com>
2023-08-25 19:11:06 -0700

    broke bone and node into a separate TU

---
+++ src/core/sm_bone.cpp
@@ -0,0 +1,306 @@
+#include "sm_types.h"
+#include "sm_bone.h"
+#include "sm_skeleton.h"
+
+using namespace std::placeholders;
+namespace r = std::ranges;
+namespace rv = std::ranges::views;
+
+/*------------------------------------------------------------------------------------------------*/
+
+namespace {
+
+	template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+	template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+}
+
+std::optional<double> apply_rotation_constraints_aux(sm::bone& b, double theta);
+
+sm::node::node(skeleton& parent, const std::string& name, double x, double y) :
+	parent_(parent),
+	name_(name),
+	x_(x),
+	y_(y)
+{}
+
+void sm::node::set_parent(bone& b) {
+	parent_ = std::ref(b);
+}
+
+void sm::node::add_child(bone& b) {
+	children_.push_back(std::ref(b));
+}
+
+std::string sm::node::name() const {
+	return name_;
+}
+
+sm::maybe_bone_ref sm::node::parent_bone() {
+	return std::visit(
+		overloaded{
+			[](skeleton_ref skel)->maybe_bone_ref {return {}; },
+			[](bone_ref bone)->maybe_bone_ref { return bone; }
+		},
+		parent_
+	);
+}
+
+// TODO: get rid of duplicate code
+sm::maybe_const_bone_ref sm::node::parent_bone() const {
+	return std::visit(
+		overloaded{
+			[](skeleton_ref skel)->maybe_const_bone_ref {return {}; },
+			[](bone_ref bone)->maybe_const_bone_ref { return bone; }
+		},
+		parent_
+	);
+}
+
+std::vector<sm::bone_ref> sm::node::child_bones() {
+	return children_;
+}
+
+std::vector<sm::const_bone_ref> sm::node::child_bones() const {
+	return children_ |
+		rv::transform([](const auto& c) {return sm::const_bone_ref(c); }) |
+		r::to< std::vector<sm::const_bone_ref>>();
+}
+
+std::vector<sm::bone_ref> sm::node::adjacent_bones() {
+	std::vector<sm::bone_ref> bones;
+	bones.reserve(children_.size() + 1);
+	if (!is_root()) {
+		bones.push_back(*parent_bone());
+	}
+	r::copy(children_, std::back_inserter(bones));
+	bones.shrink_to_fit();
+	return bones;
+}
+
+std::vector<sm::const_bone_ref> sm::node::adjacent_bones() const {
+	auto* non_const_this = const_cast<sm::node*>(this);
+	return non_const_this->adjacent_bones() |
+		rv::transform([](const auto& c) {return sm::const_bone_ref(c); }) |
+		r::to< std::vector<sm::const_bone_ref>>();
+}
+
+sm::skeleton_ref sm::node::owner() {
+	return std::visit(
+		overloaded{
+			[](skeleton_ref skel) {return skel; },
+			[](bone_ref bone) {
+				return bone.get().owner();
+			}
+		},
+		parent_
+	);
+}
+
+sm::const_skeleton_ref sm::node::owner() const {
+	auto non_const_this = const_cast<sm::node*>(this);
+	return non_const_this->owner();
+}
+
+double sm::node::world_x() const {
+	return x_;
+}
+
+double sm::node::world_y() const {
+	return y_;
+}
+
+void sm::node::set_world_pos(const point& pt) {
+	x_ = pt.x;
+	y_ = pt.y;
+}
+
+sm::point sm::node::world_pos() const {
+	return { x_, y_ };
+}
+
+std::any sm::node::get_user_data() const {
+	return user_data_;
+}
+
+void sm::node::set_user_data(std::any data) {
+	user_data_ = data;
+}
+
+bool sm::node::is_root() const {
+	return std::holds_alternative<skeleton_ref>(parent_);
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+sm::bone::bone(std::string name, sm::node& u, sm::node& v) :
+	name_(name), u_(u), v_(v) {
+	v.set_parent(*this);
+	u.add_child(*this);
+	length_ = scaled_length();
+}
+
+sm::result sm::bone::set_rotation_constraint(double min, double max, bool relative_to_parent) {
+	if (relative_to_parent && !parent_bone()) {
+		return result::no_parent;
+	}
+	rot_constraint_ = rot_constraint{ relative_to_parent, min, max };
+	return result::success;
+}
+
+std::optional<sm::rot_constraint> sm::bone::rotation_constraint() const {
+	return rot_constraint_;
+}
+
+void sm::bone::remove_rotation_constraint() {
+	rot_constraint_ = {};
+}
+
+std::string sm::bone::name() const {
+	return name_;
+}
+
+sm::node& sm::bone::parent_node() {
+	return u_;
+}
+
+const sm::node& sm::bone::parent_node() const {
+	auto* non_const_this = const_cast<sm::bone*>(this);
+	return non_const_this->parent_node();
+}
+
+sm::node& sm::bone::child_node() {
+	return v_;
+}
+
+const sm::node& sm::bone::child_node() const {
+	auto* non_const_this = const_cast<sm::bone*>(this);
+	return non_const_this->child_node();
+}
+
+sm::node& sm::bone::opposite_node(const node& j) {
+	if (&j == &u_) {
+		return v_;
+	}
+	else {
+		return u_;
+	}
+}
+
+std::vector<sm::bone_ref> sm::bone::child_bones() {
+	return v_.child_bones();
+}
+
+std::vector<sm::const_bone_ref> sm::bone::child_bones() const {
+	return const_cast<const sm::node&>(v_).child_bones();
+}
+
+std::vector<sm::bone_ref> sm::bone::sibling_bones() {
+	return u_.child_bones() |
+		rv::filter(
+			[this](bone_ref sib) {
+				return &sib.get() != this;
+			}
+	) | r::to< std::vector<sm::bone_ref>>();
+}
+
+const sm::node& sm::bone::opposite_node(const node& j) const {
+	auto* non_const_this = const_cast<sm::bone*>(this);
+	return non_const_this->opposite_node(j);
+}
+
+sm::skeleton_ref sm::bone::owner() {
+	return parent_node().owner();
+}
+
+sm::const_skeleton_ref sm::bone::owner() const {
+	return parent_node().owner();
+}
+
+std::optional<sm::node_ref> sm::bone::shared_node(const bone& b) {
+	auto* b_u = &b.parent_node();
+	auto* b_v = &b.child_node();
+	if (&u_ == b_u || (&u_ == b_v)) {
+		return u_;
+	}
+	else if (&v_ == b_u || &v_ == b_v) {
+		return v_;
+	}
+	return {};
+}
+
+std::optional<sm::const_node_ref> sm::bone::shared_node(const bone& b) const {
+	auto* non_const_this = const_cast<sm::bone*>(this);
+	return non_const_this->shared_node(b);
+}
+
+std::tuple<sm::point, sm::point> sm::bone::line_segment() const {
+	return { u_.world_pos(), v_.world_pos() };
+}
+
+double sm::bone::length() const {
+	return length_;
+}
+
+double sm::bone::scaled_length() const {
+	return std::apply(distance, line_segment());
+}
+
+double sm::bone::world_rotation() const {
+	auto [u, v] = line_segment();
+	return std::atan2(
+		(v.y - u.y),
+		(v.x - u.x)
+	);
+}
+
+double sm::bone::rotation() const {
+	auto parent = parent_bone();
+	if (!parent) {
+		return world_rotation();
+	}
+	return world_rotation() - parent->get().world_rotation();
+}
+
+double sm::bone::scale() const {
+	auto parent = parent_bone();
+	if (!parent) {
+		return absolute_scale();
+	}
+	return absolute_scale() / parent->get().absolute_scale();
+}
+
+double sm::bone::absolute_scale() const {
+	return scaled_length() / length();
+}
+
+sm::maybe_const_bone_ref  sm::bone::parent_bone() const {
+	return u_.parent_bone();
+}
+
+sm::maybe_bone_ref  sm::bone::parent_bone() {
+	return u_.parent_bone();
+}
+
+std::any sm::bone::get_user_data() const {
+	return user_data_;
+}
+
+void sm::bone::set_user_data(std::any data) {
+	user_data_ = data;
+}
+
+void sm::bone::rotate(double theta) {
+
+	auto new_theta = apply_rotation_constraints_aux(*this, theta + world_rotation());
+	theta = new_theta ? *new_theta - world_rotation() : theta;
+
+	auto rotate_mat = rotate_about_point_matrix(u_.world_pos(), theta);
+
+	auto rotate_about_u = [&rotate_mat](node& j)->bool {
+		j.set_world_pos(transform(j.world_pos(), rotate_mat));
+		return true;
+	};
+
+	visit_nodes(v_, rotate_about_u);
+}
--- src/core/sm_skeleton.h
+++ src/core/sm_skeleton.h
@@ -13,34 +13,11 @@
 #include <ranges>
 #include <variant>
 #include "sm_types.h"
+#include "sm_bone.h"
 
 /*------------------------------------------------------------------------------------------------*/
 
 namespace sm {
-
-    namespace detail {
-        template <typename T> class enable_protected_make_unique {
-        protected:
-            template <typename... Args>
-            static std::unique_ptr<T> make_unique(Args &&... args) {
-                class make_unique_enabler : public T {
-                public:
-                    make_unique_enabler(Args &&... args) :
-                        T(std::forward<Args>(args)...) {}
-                };
-                return std::make_unique<make_unique_enabler>(std::forward<Args>(args)...);
-            }
-        };
-
-		template<typename T>
-		auto to_range_view(auto& tbl) {
-			return tbl | std::ranges::views::transform(
-				[](auto& pair)->T {
-					return *pair.second;
-				}
-			);
-		}
-    }
 
 	class skeleton : public detail::enable_protected_make_unique<skeleton> {
 		friend class world;
@@ -71,110 +48,6 @@ namespace sm {
 		auto bones() const { return detail::to_range_view<const_bone_ref>(bones_); }
 	};
 
-    class node : public detail::enable_protected_make_unique<node> {
-        friend class world;
-        friend class bone;
-		friend class world;
-    private:
-        std::string name_;
-        double x_;
-        double y_;
-        std::variant<skeleton_ref, bone_ref> parent_;
-        std::vector<bone_ref> children_;
-        std::any user_data_;
-
-    protected:
-
-        node(skeleton& parent, const std::string& name, double x, double y);
-        void set_parent(bone& b);
-        void add_child(bone& b);
-
-    public:
-        std::string name() const;
-
-		maybe_bone_ref parent_bone();
-        maybe_const_bone_ref parent_bone() const;
-
-        std::vector<const_bone_ref> child_bones() const;
-		std::vector<bone_ref> child_bones();
-
-		std::vector<const_bone_ref> adjacent_bones() const;
-		std::vector<bone_ref> adjacent_bones();
-
-		skeleton_ref owner();
-		const_skeleton_ref owner() const;
-
-        double world_x() const;
-        double world_y() const;
-        void set_world_pos(const point& pt);
-        point world_pos() const;
-
-        std::any get_user_data() const;
-        bool is_root() const;
-        
-		void set_user_data(std::any data);
-    };
-
-    class bone : public detail::enable_protected_make_unique<bone> {
-        friend class world;
-    private:
-
-        std::string name_;
-        node& u_;
-        node& v_;
-        double length_;
-        std::any user_data_;
-		std::optional<rot_constraint> rot_constraint_;
-
-    protected:
-
-        bone(std::string name, node& u, node& v);
-
-    public:
-        std::string name() const;
-
-		maybe_const_bone_ref parent_bone() const;
-		maybe_bone_ref parent_bone();
-
-		std::vector<bone_ref> child_bones();
-		std::vector<const_bone_ref> child_bones() const;
-
-		std::vector<bone_ref> sibling_bones();
-
-        const node& parent_node() const;
-		const node& child_node() const;
-		const node& opposite_node(const node& j) const;
-
-		skeleton_ref owner();
-		const_skeleton_ref owner() const;
-
-		node& parent_node();
-		node& child_node();
-		node& opposite_node(const node& j);
-
-		std::optional<const_node_ref> shared_node(const bone& b) const;
-		std::optional<node_ref> shared_node(const bone& b);
-
-		std::optional<rot_constraint> rotation_constraint() const;
-		result set_rotation_constraint(double start, double span, bool relative_to_parent);
-		void remove_rotation_constraint();
-
-        std::tuple<point, point> line_segment() const;
-        double length() const;
-        double scaled_length() const;
-        double world_rotation() const;
-        double rotation() const;
-        double scale() const;
-        double absolute_scale() const;
-        std::any get_user_data() const;
-        void set_user_data(std::any data);
-
-        void rotate(double theta);
-    };
-
-    using node_visitor = std::function<bool(node&)>;
-    using bone_visitor = std::function<bool(bone&)>;
-
     class world {
 		friend class skeleton;
     private:
@@ -198,6 +71,9 @@ namespace sm {
 		auto skeletons() { return detail::to_range_view<skeleton_ref>(skeletons_); }
 		auto skeletons() const { return detail::to_range_view<const_skeleton_ref>(skeletons_); }
     };
+
+	using node_visitor = std::function<bool(node&)>;
+	using bone_visitor = std::function<bool(bone&)>;
 
     void dfs(node& j1, node_visitor visit_node = {}, bone_visitor visit_bone = {},
         bool just_downstream = false);
