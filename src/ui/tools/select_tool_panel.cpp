965aaf96a9517b3b42a839ce9bf588e417aec5d7
Joe Wezorek <jwezorek@gmail.com>
2024-01-05 12:38:40 -0800

    select tool panel ui pretty much done

--- src/ui/tools/select_tool_panel.cpp
+++ src/ui/tools/select_tool_panel.cpp
@@ -16,13 +16,59 @@ namespace {
     }
 }
 
+std::vector<QWidget*> ui::tool::select_tool_panel::rot_ctrls(bool include_master) {
+    std::vector<QWidget*> ctrls = {
+        rotate_on_pin_, rot_rag_doll_mode_,
+        rot_rubber_band_mode_, rot_rigid_mode_
+    };
+    if (include_master) {
+        ctrls.push_back(rotate_);
+    }
+    return ctrls;
+}
+
+std::vector<QWidget*> ui::tool::select_tool_panel::trans_ctrls(bool include_master) {
+    std::vector<QWidget*> ctrls = { 
+        trans_selection_, trans_rag_doll_mode_,
+        trans_rubber_band_mode_, trans_rigid_mode_
+    };
+    if (include_master) {
+        ctrls.push_back(translate_);
+    }
+    return ctrls;
+}
+
+ui::tool::sel_drag_mode ui::tool::select_tool_panel::rot_mode() const {
+    static std::unordered_map<QAbstractButton*, sel_drag_mode> btns;
+    if (btns.empty()) {
+        btns = {
+            {rot_rag_doll_mode_, sel_drag_mode::rag_doll},
+            {rot_rubber_band_mode_, sel_drag_mode::rubber_band},
+            {rot_rigid_mode_, sel_drag_mode::rigid}
+        };
+    }
+    return btns[rotate_group_->checkedButton()];
+}
+
+ui::tool::sel_drag_mode ui::tool::select_tool_panel::trans_mode() const {
+    static std::unordered_map<QAbstractButton*, sel_drag_mode> btns;
+    if (btns.empty()) {
+        btns = { 
+            {trans_rag_doll_mode_, sel_drag_mode::rag_doll},
+            {trans_rubber_band_mode_, sel_drag_mode::rubber_band},
+            {trans_rigid_mode_, sel_drag_mode::rigid}
+        };
+    }
+    return btns[translate_group_->checkedButton()];
+}
+
 ui::tool::select_tool_panel::select_tool_panel() : QWidget() {
 
     QVBoxLayout* column = new QVBoxLayout(this);
     
-    QButtonGroup* toplevel_behavior = new QButtonGroup(this);
-    QButtonGroup* translate_mode = new QButtonGroup(this);
-    QButtonGroup* rotate_mode = new QButtonGroup(this);
+    toplevel_group_ = new QButtonGroup(this);
+    translate_group_ = new QButtonGroup(this);
+    rotate_group_ = new QButtonGroup(this);
     
     column->addWidget(drag_behaviors_ = new QCheckBox("drag behaviors on"));
     
@@ -39,19 +85,92 @@ ui::tool::select_tool_panel::select_tool_panel() : QWidget() {
     column->addLayout(indented_widget(2, trans_rigid_mode_ = new QRadioButton("rigid mode")));
     column->addStretch();
 
-    toplevel_behavior->addButton( rotate_ );
-    toplevel_behavior->addButton( translate_ );
+    toplevel_group_->addButton( rotate_ );
+    toplevel_group_->addButton( translate_ );
 
-    translate_mode->addButton( trans_rag_doll_mode_ );
-    translate_mode->addButton( trans_rubber_band_mode_ );
-    translate_mode->addButton( trans_rigid_mode_ );
+    translate_group_->addButton( trans_rag_doll_mode_ );
+    translate_group_->addButton( trans_rubber_band_mode_ );
+    translate_group_->addButton( trans_rigid_mode_ );
 
-    rotate_mode->addButton( rot_rag_doll_mode_ );
-    rotate_mode->addButton( rot_rubber_band_mode_ );
-    rotate_mode->addButton( rot_rigid_mode_ );
+    rotate_group_->addButton( rot_rag_doll_mode_ );
+    rotate_group_->addButton( rot_rubber_band_mode_ );
+    rotate_group_->addButton( rot_rigid_mode_ );
+
+    connect(rotate_, &QRadioButton::toggled,
+        [this](bool checked) {
+            if (checked) {
+                for (auto* rot :rot_ctrls(false)) {
+                    rot->setEnabled(true);
+                }
+                for (auto* trans : trans_ctrls(false)) {
+                    trans->setEnabled(false);
+                }
+            } else {
+                for (auto* rot : rot_ctrls(false)) {
+                    rot->setEnabled(false);
+                }
+                for (auto* trans : trans_ctrls(false)) {
+                    trans->setEnabled(true);
+                }
+            }
+        }
+    );
+
+    connect(drag_behaviors_, &QCheckBox::toggled,
+        [this](bool checked) {
+            if (checked) {
+                rotate_->setEnabled(true);
+                translate_->setEnabled(true);
+                if (rotate_->isChecked()) {
+                    for (auto* rot : rot_ctrls(false)) {
+                        rot->setEnabled(true);
+                    }
+                } else {
+                    for (auto* trans : trans_ctrls(false)) {
+                        trans->setEnabled(false);
+                    }
+                }
+            }
+            else {
+                for (auto* rot : rot_ctrls(true)) {
+                    rot->setEnabled(false);
+                }
+                for (auto* trans : trans_ctrls(true)) {
+                    trans->setEnabled(false);
+                }
+            }
+        }
+    );
+
+    init();
 }
 
+void ui::tool::select_tool_panel::init() {
+    drag_behaviors_->setChecked(true);
+    translate_->setChecked(true);
+    trans_rigid_mode_->setChecked(true);
+    rot_rigid_mode_->setChecked(true);
 
+    for (auto* rot : rot_ctrls(false)) {
+        rot->setEnabled(false);
+    }
+    for (auto* trans : trans_ctrls(false)) {
+        trans->setEnabled(true);
+    }
+}
+
+std::optional<ui::tool::sel_drag_settings> ui::tool::select_tool_panel::settings() const {
+    if (!drag_behaviors_->isChecked()) {
+        return {};
+    }
+    return {{
+        .is_in_rotate_mode_ = rotate_->isChecked(),
+        .rotate_on_pin_ = rotate_on_pin_->isChecked(),
+        .rotate_mode_ = rot_mode(),
+        .trans_sel_ = trans_selection_->isChecked(),
+        .trans_mode_ = trans_mode()
+    }};
+}
 
 
 
--- src/ui/tools/select_tool_panel.h
+++ src/ui/tools/select_tool_panel.h
@@ -1,25 +1,54 @@
 #pragma once
 
 #include "tool.h"
+#include <optional>
 
 namespace ui {
 
     namespace tool {
 
+        enum class sel_drag_mode {
+            rag_doll,
+            rubber_band,
+            rigid
+        };
+
+        struct sel_drag_settings {
+            bool is_in_rotate_mode_;
+            bool rotate_on_pin_;
+            sel_drag_mode rotate_mode_;
+            bool trans_sel_;
+            sel_drag_mode trans_mode_;
+        };
+      
+
         class select_tool_panel : public QWidget {
+
             QCheckBox* drag_behaviors_;
             QRadioButton* rotate_;
             QCheckBox* rotate_on_pin_;
+            QRadioButton* rot_rag_doll_mode_;
+            QRadioButton* rot_rubber_band_mode_;
+            QRadioButton* rot_rigid_mode_;
             QRadioButton* translate_;
             QCheckBox* trans_selection_;
             QRadioButton* trans_rag_doll_mode_;
             QRadioButton* trans_rubber_band_mode_;
             QRadioButton* trans_rigid_mode_;
-            QRadioButton* rot_rag_doll_mode_;
-            QRadioButton* rot_rubber_band_mode_;
-            QRadioButton* rot_rigid_mode_;
+
+            QButtonGroup* toplevel_group_;
+            QButtonGroup* translate_group_;
+            QButtonGroup* rotate_group_;
+
+            std::vector<QWidget*> rot_ctrls(bool include_master);
+            std::vector<QWidget*> trans_ctrls(bool include_master);
+            sel_drag_mode rot_mode() const;
+            sel_drag_mode trans_mode() const;
+
         public:
             select_tool_panel();
+            void init();
+            std::optional<sel_drag_settings> settings() const;
         };
 
     }
