d349e833ea266194c78ad634c1d76aef5f8b0803
joe wezorek <jwezorek@gmail.com>
2024-05-04 10:46:01 -0700

    removed old move tool code, e.g. fabrik etc., from what is now going to be the animate tool.

--- src/ui/tools/animate_tool.h
+++ src/ui/tools/animate_tool.h
@@ -7,20 +7,6 @@ namespace ui {
 
         class animate : public base {
         public:
-
-            struct move_state {
-
-                canvas::scene* canvas_;
-                ui::canvas::item::node* anchor_;
-                ui::canvas::item::bone* bone_;
-                QGraphicsSceneMouseEvent* event_;
-                QKeyEvent* key_event_;
-
-                move_state();
-                void set(canvas::scene& canvas, QGraphicsSceneMouseEvent* event);
-                void set(canvas::scene& canvas, QKeyEvent* event);
-            };
-
             animate();
             void keyPressEvent(canvas::scene& c, QKeyEvent* event) override;
             void mousePressEvent(canvas::scene& c, QGraphicsSceneMouseEvent* event) override;
@@ -29,9 +15,6 @@ namespace ui {
             QWidget* settings_widget() override;
 
         private:
-            QWidget* settings_;
-            QButtonGroup* btns_;
-            move_state state_;
         };
     }
 }
