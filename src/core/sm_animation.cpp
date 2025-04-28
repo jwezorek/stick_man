f72c341f762d6e5699f3c6372c498eec482176a6
joe wezorek <jwezorek@gmail.com>
2025-04-28 09:29:11 -0700

    refactored animation stubs

--- src/core/sm_animation.cpp
+++ src/core/sm_animation.cpp
@@ -1,21 +1,67 @@
 #include "sm_animation.h"
+#include "sm_skeleton.h"
+#include <algorithm>
 
-/*------------------------------------------------------------------------------------------------*/
+namespace  {
 
-sm::animation::animation(const std::string& name) : name_(name) {
+} 
 
+void sm::pose::set_node_position(const std::string& node_name, const sm::point& pos) {
+    node_positions_[node_name] = pos;
 }
 
-void sm::animation::insert(int start_time, const animation_event& event) {
-    timeline_[start_time].push_back(event);
+std::optional<sm::point> sm::pose::node_position(const std::string& node_name) const {
+    auto it = node_positions_.find(node_name);
+    if (it != node_positions_.end()) {
+        return it->second;
+    }
+    return std::nullopt;
 }
 
-void sm::animation::set(int start_time, const animation_event& event, int index) {
-    timeline_.at(start_time).at(index) = event;
+void sm::pose::apply(sm::skeleton& skel) const {
+    // TODO: Apply the pose to the skeleton nodes.
 }
 
-std::vector<sm::animation_event> sm::animation::events(int start_time) const {
-    return timeline_.at(start_time);
+sm::animation_script::animation_script(const std::string& name) : name_(name) {}
+
+void sm::animation_script::insert(int start_time_ms, const sm::animation_event& event) {
+    timeline_[start_time_ms].push_back(event);
 }
 
+void sm::animation_script::set(int start_time_ms, const sm::animation_event& event, int index) {
+    if (timeline_.contains(start_time_ms) && index < static_cast<int>(timeline_.at(start_time_ms).size())) {
+        timeline_.at(start_time_ms)[index] = event;
+    }
+}
+
+std::vector<sm::animation_event> sm::animation_script::events(int start_time_ms) const {
+    if (timeline_.contains(start_time_ms)) {
+        return timeline_.at(start_time_ms);
+    }
+    return {};
+}
+
+const std::string& sm::animation_script::name() const {
+    return name_;
+}
+
+sm::animation sm::animation_script::bake(const sm::skeleton& skel) const {
+    // TODO: Bake the script into a compiled animation.
+    return sm::animation(name_);
+}
+
+sm::animation::animation(const std::string& name) : name_(name) {}
+
+sm::pose sm::animation::pose_at_time(int time_ms) const {
+    // TODO: Return the pose at the given time (stub for now)
+    return sm::pose();
+}
+
+const std::string& sm::animation::name() const {
+    return name_;
+}
+
+void sm::animation::add_keyframe_internal(int time_ms, const sm::pose& p) {
+    keyframes_.emplace_back(time_ms, p);
+}
 
--- src/core/sm_animation.h
+++ src/core/sm_animation.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#pragma once
+
 #include "sm_types.h"
 #include <string>
+#include <unordered_map>
+#include <vector>
 #include <variant>
 #include <map>
+#include <optional>
 
 namespace sm {
 
@@ -11,13 +16,13 @@ namespace sm {
         std::string axis;
         std::string rotor;
         double theta;
-        int duration;
+        int duration_ms;
     };
 
     struct translation {
         std::string subject;
         point offset;
-        int duration;
+        int duration_ms;
     };
 
     template <typename T>
@@ -25,20 +30,51 @@ namespace sm {
 
     using animation_event = std::variant<rotation, translation>;
 
-    class animation {
-        friend class skeleton;
+    class pose {
+    private:
+        std::unordered_map<std::string, sm::point> node_positions_;
 
+    public:
+        pose() = default;
+
+        void set_node_position(const std::string& node_name, const sm::point& pos);
+        std::optional<sm::point> node_position(const std::string& node_name) const;
+
+        void apply(skeleton& skel) const;
+    };
+
+    class animation;
+
+    class animation_script {
+    private:
         std::string name_;
         std::map<int, std::vector<animation_event>> timeline_;
 
     public:
+        animation_script(const std::string& name);
 
-        animation(const std::string& name);
+        void insert(int start_time_ms, const animation_event& event);
+        void set(int start_time_ms, const animation_event& event, int index);
+        std::vector<animation_event> events(int start_time_ms) const;
 
-        void insert(int start_time, const animation_event& event);
-        void set(int start_time, const animation_event& event, int index);
-        std::vector<animation_event> events(int start_time) const;
+        const std::string& name() const;
 
+        animation bake(const skeleton& skel) const;
     };
 
-}
+    class animation {
+    private:
+        friend class animation_script;
+
+        std::string name_;
+        std::vector<std::pair<int, pose>> keyframes_;
+
+        animation(const std::string& name);
+        void add_keyframe_internal(int time_ms, const pose& p);
+
+    public:
+        pose pose_at_time(int time_ms) const;
+        const std::string& name() const;
+    };
+
+} 
